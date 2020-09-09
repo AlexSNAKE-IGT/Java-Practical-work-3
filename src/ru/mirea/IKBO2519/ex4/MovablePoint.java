190157fbfdffbf04a5388267ef92fe2b56b4ca29
Alexnader Makarov <alexgamechanel2001@gmail.com>
2020-09-09 13:52:50 +0300

    Изменения в 4 упражнении

--- src/ru/mirea/IKBO2519/ex4/MovablePoint.java
+++ src/ru/mirea/IKBO2519/ex4/MovablePoint.java
@@ -1,9 +1,9 @@
 package ru.mirea.IKBO2519.ex4;
 
 public class MovablePoint implements Movable {
-    public int x;
-    public int y;
-    public int xSpeed;
+    int x;
+    int y;
+    int xSpeed;
     int ySpeed;
     MovablePoint(int x,int y,int xSpeed,int ySpeed)
     {
